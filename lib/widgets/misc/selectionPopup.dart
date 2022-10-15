3adc8f8f72215d46300a9e7466c7bfe12c628ecb
ArthurHeitmann <art.tec.15@gmail.com>
2022-10-15 22:16:55 +0200

    fixed selection popup on screen edges

--- lib/widgets/misc/selectionPopup.dart
+++ lib/widgets/misc/selectionPopup.dart
@@ -1,5 +1,6 @@
 
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -52,39 +53,55 @@ class _SelectionContextMenu extends StatefulWidget {
 }
 
 class _SelectionContextMenuState extends State<_SelectionContextMenu> {
+  static const popupWidth = 300.0;
+  static const entryHeight = 28.0;
+  static const screenPadding = 10.0;
+
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var expectedHeight = widget.configs.length * entryHeight;
+    var leftOffset = widget.pos.dx;
+    var topOffset = widget.pos.dy;
+
+    if (leftOffset + popupWidth + screenPadding > screenSize.width)
+      leftOffset = max(0, screenSize.width - popupWidth - screenPadding);
+    if (topOffset + expectedHeight + screenPadding > screenSize.height)
+      topOffset = max(0, screenSize.height - expectedHeight - screenPadding);
     return Stack(
       children: [
         Positioned(
-          left: widget.pos.dx,
-          top: widget.pos.dy,
+          left: leftOffset,
+          top: topOffset,
           child: Material(
             elevation: 5,
             borderRadius: BorderRadius.circular(5),
             clipBehavior: Clip.antiAlias,
             color: getTheme(context).contextMenuBgColor,
             child: ConstrainedBox(
-              constraints: BoxConstraints.tightFor(width: 300),
+              constraints: BoxConstraints.tightFor(width: popupWidth),
               child: Column(
-                children: widget.configs.map((e) => TextButton.icon(
-                  autofocus: widget.configs.indexOf(e) == 0,
-                  icon: Icon(e.icon, size: 22,),
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all(getTheme(context).textColor),
-                  ),
-                  label: SizedBox(
-                    height: 25,
-                    child: Row(
-                      children: [
-                        Expanded(child: Text(e.name)),
-                      ],
+                children: widget.configs.map((e) => SizedBox(
+                  height: entryHeight,
+                  child: TextButton.icon(
+                    autofocus: widget.configs.indexOf(e) == 0,
+                    icon: Icon(e.icon, size: 22,),
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all(getTheme(context).textColor),
                     ),
+                    label: SizedBox(
+                      height: 25,
+                      child: Row(
+                        children: [
+                          Expanded(child: Text(e.name)),
+                        ],
+                      ),
+                    ),
+                    onPressed: () {
+                      widget.completer.complete(e.getValue());
+                      Navigator.of(context).pop();
+                    },
                   ),
-                  onPressed: () {
-                    widget.completer.complete(e.getValue());
-                    Navigator.of(context).pop();
-                  },
                 )).toList(),
               ),
             ),
