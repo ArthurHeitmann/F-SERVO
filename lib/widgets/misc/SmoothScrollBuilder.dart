601aad615698379ea0d936bf1e5c102441bbf777
ArthurHeitmann <art.tec.15@gmail.com>
2023-02-13 08:20:35 +0100

    Replaced SmoothScrollBuilder with DynMouseScroll package

--- lib/widgets/misc/SmoothScrollBuilder.dart
+++ lib/widgets/misc/SmoothScrollBuilder.dart
@@ -1,9 +1,7 @@
-import 'dart:async';
 
-import 'package:flutter/gestures.dart';
+import 'package:dyn_mouse_scroll/dyn_mouse_scroll.dart';
 import 'package:flutter/material.dart';
 
-import '../../utils/utils.dart';
 
 class SmoothScrollBuilder extends StatefulWidget {
   final ScrollController controller;
@@ -26,52 +24,17 @@ class SmoothScrollBuilder extends StatefulWidget {
 }
 
 class _SmoothScrollBuilderState extends State<SmoothScrollBuilder> {
-  double targetOffset = 0;
-  bool overrideScrollBehavior = true;
-  bool isScrolling = false;
-  Timer? isScrollingTimer;
-
-  @override
-  void initState() {
-    if (widget.controller.hasClients)
-      targetOffset = widget.controller.offset;
-    widget.controller.addListener(onScrollChange);
-    super.initState();
-  }
-
-  void onScrollChange() {
-    if (isScrolling)
-      return;
-    targetOffset = widget.controller.offset;
-  }
-
-  void onWheelScroll(PointerScrollEvent event) {
-    if (!overrideScrollBehavior)
-      setState(() => overrideScrollBehavior = true);
-    targetOffset += widget.stepSize * event.scrollDelta.dy.sign;
-    targetOffset = clamp(targetOffset, 0, widget.controller.position.maxScrollExtent);
-    if (targetOffset == widget.controller.offset)
-      return;
-    isScrolling = true;
-    widget.controller.animateTo(targetOffset, duration: widget.duration, curve: Curves.linear);
-    isScrolling = true;
-    isScrollingTimer?.cancel();
-    isScrollingTimer = Timer(widget.duration, () => isScrolling = false);
-  }
-
-  void onContinuosScroll(double dy) {
-    if (overrideScrollBehavior)
-      setState(() => overrideScrollBehavior = false);
-  }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onPanUpdate: (event) => onContinuosScroll(event.delta.dy),
-      child: Listener(
-        onPointerSignal: (event) => event is PointerScrollEvent ? onWheelScroll(event) : null,
-        child: widget.builder(context, widget.controller, overrideScrollBehavior ? const NeverScrollableScrollPhysics() : widget.physics),
-      ),
+    return DynMouseScroll(
+      durationMS: widget.duration.inMilliseconds,
+      mobilePhysics: widget.physics ?? const BouncingScrollPhysics(),
+      builder: (context, controller, physics) => widget.builder(
+        context,
+        controller,
+        physics,
+      )
     );
   }
 }
