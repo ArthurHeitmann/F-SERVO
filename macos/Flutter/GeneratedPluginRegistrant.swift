1be7504b6410c857182dfc9ef68e8b76d0f20602
ArthurHeitmann <art.tec.15@gmail.com>
2023-02-13 08:23:37 +0100

    added missing dependency files

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,9 @@ import Foundation
 import audioplayers_darwin
 import desktop_drop
 import flutter_window_close
-import path_provider_macos
+import path_provider_foundation
 import screen_retriever
-import shared_preferences_macos
+import shared_preferences_foundation
 import url_launcher_macos
 import window_manager
 
